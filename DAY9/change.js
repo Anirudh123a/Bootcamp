45cb1a1f119682e9fb468dd53480fd8f0b5656e9
Anirudh123a <anirudhjb2003@gmail.com>
2025-08-18 14:47:05 +0530

    push

--- DAY9/change.js
+++ DAY9/change.js
@@ -1,19 +1,24 @@
     const con=document.getElementById('container');
     const arr=[
+            "asset/Berserk\ Wallpaper\ QHD\ _\ Sfondo\ bianco\ e\ nero\ iphone\ Arte\ dei\ fumetti\ Sfondi\ per\ cellulare.jpg",
+
         "asset/one-piece-luffy-gear-5-3840x2160-10747.jpg",
         "asset/Pin de Antonio Pizzo em Spiderman em 2025 _ Imagem de fundo de computador Papel de parede do notebook Ideias de papel de parede.jpg",
     "asset/tanjiro-kamado-red-5120x2880-22577.png",
-    "asset/Berserk\ Wallpaper\ QHD\ _\ Sfondo\ bianco\ e\ nero\ iphone\ Arte\ dei\ fumetti\ Sfondi\ per\ cellulare.jpg",
     "asset/Van Gogh Starry Night Wallpaper.jpg",
 
 
     ];
+const img=document.getElementsByClassName('active')
+img[0].classList.remove('active');
     let i=0;
+  
     let nextimg=()=>{
            i++;
         if(i==5){
             i=0
         }
+          img[0].classList.add('active');
         con.style.backgroundImage=`url('${arr[i]}')`;
      
     }
